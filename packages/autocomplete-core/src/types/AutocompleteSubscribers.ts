1c978b6e15c84093f2c48c8b6c1f02c886e1fd65
vsamaru <vsamaru@gmail.com>
2023-02-15 19:49:12 +0000

    first commit

---
+++ packages/autocomplete-core/src/types/AutocompleteSubscribers.ts
@@ -0,0 +1,16 @@
+import { BaseItem } from './AutocompleteApi';
+import {
+  OnActiveParams,
+  OnResolveParams,
+  OnSelectParams,
+} from './AutocompleteSource';
+
+export type AutocompleteSubscriber<TItem extends BaseItem> = {
+  onSelect(params: OnSelectParams<TItem>): void;
+  onActive(params: OnActiveParams<TItem>): void;
+  onResolve(params: OnResolveParams<TItem>): void;
+};
+
+export type AutocompleteSubscribers<TItem extends BaseItem> = Array<
+  Partial<AutocompleteSubscriber<TItem>>
+>;
